414f35bf08e5bb884b1cdd8cea3165e9b0dcc1f1
Dan Beck <63600571+db1721@users.noreply.github.com>
2023-07-13 22:12:35 -0400

    Completed main components

--- movie-rater/src/app/app.module.ts
+++ movie-rater/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { AuthModule } from './auth/auth.module';
+import { MainModule } from './main/main.module';
 
 import { AppComponent } from './app.component';
 
+const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'movies'}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthModule,
+    MainModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
